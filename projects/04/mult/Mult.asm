01000cc52f07049a70009932ec2620938a72698e
jipark <jipark7937@gmail.com>
2022-01-31 09:13:17 +0000

    project3 - Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,21 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @R2
+    M=0      // reset R2
+(LOOP)
+    @R1
+    D=M
+    @END
+    D;JLE    // if (R1 <= 0) END LOOP
+    @R0
+    D=M
+    @R2
+    M=M+D
+    @R1
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
