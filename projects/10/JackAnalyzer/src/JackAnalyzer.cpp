9f14dc7c87b5ba128cebbe3844983f12de5c6a95
jipark <jipark7937@gmail.com>
2022-04-14 12:11:12 +0000

    project10 - JackAnalyzer 모듈 구현 구성

---
+++ projects/10/JackAnalyzer/src/JackAnalyzer.cpp
@@ -0,0 +1,19 @@
+/**
+    Implementation of JackAnalyzer.h
+*/
+
+#include "JackAnalyzer.h"
+
+/* =========== PUBLIC ============= */
+
+JackAnalyzer::JackAnalyzer(const std::string& path) {
+    // path에 포함된 jack파일들 경로를 불러와 저장한다.
+}
+
+JackAnalyzer::~JackAnalyzer() {
+}
+
+void JackAnalyzer::translate() {
+    // 입력 데이터를 JackTokenizer 모듈로 토큰화한다.
+    // 토큰들을 CompilationEngine 모듈에 전달해 컴파일한 후 출력 메시지를 전달한다.
+}
