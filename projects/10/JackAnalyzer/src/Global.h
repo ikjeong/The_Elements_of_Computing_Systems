25e4bd62d465e1c4bca71856cd91131248eac4d1
jipark <jipark7937@gmail.com>
2022-04-14 11:54:59 +0000

    project10 - Global header

---
+++ projects/10/JackAnalyzer/src/Global.h
@@ -0,0 +1,14 @@
+/**
+    Global Constants and Header, Exception Class
+*/
+
+#ifndef __GLOBAL_H__
+#define __GLOBAL_H__
+
+#include <iostream>
+#include <fstream>
+#include <string>
+/* If gcc version is under 9, use '-lstdc++fs' */
+#include <filesystem>
+
+#endif
