9b26a60343a8835c16ff030678b932038627f4a9
jipark <jipark7937@gmail.com>
2022-04-14 11:37:45 +0000

    project10 - main.cpp 구성

---
+++ projects/10/JackAnalyzer/src/main.cpp
@@ -0,0 +1,27 @@
+/**
+    Main Jack Analyzer
+    v1: Implement parsing program
+
+    Modules:
+    - JackAnalyzer: Top module to set up and load other modules.
+    - JackTokenizer: Tokenization module
+    - CompilationEngine: Recursive Down Parser
+
+    How to use
+    prompt> JackAnaylzer source
+    source is .jack file or directory which contains .jack files.
+    return .xml file.
+*/
+
+#include "JackAnalyzer.h"
+
+int main(int argc, char* argv[]) {
+    try {
+        // JackAnalyzer에 입력 파일 경로를 제공하여 불러와 설정한다.
+        // JackAnalyzer의 구문 분석을 실행시킨다.
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    return 0;
+}
