6d38a8a25010804de603b560329910fde7348893
jipark <jipark7937@gmail.com>
2022-04-15 14:18:03 +0000

    project10 - exception of no jack files

--- projects/10/JackAnalyzer/src/Global.h
+++ projects/10/JackAnalyzer/src/Global.h
@@ -12,4 +12,10 @@
 /* If gcc version is under 9, use '-lstdc++fs' */
 #include <filesystem>
 
+class file_exception : public std::runtime_error {
+public:
+    file_exception(const std::string& path)
+    : runtime_error("File Exception: fail to load file(Path: " + path + ").") { }
+};
+
 #endif
--- projects/10/JackAnalyzer/src/JackAnalyzer.cpp
+++ projects/10/JackAnalyzer/src/JackAnalyzer.cpp
@@ -37,6 +37,7 @@ JackAnalyzer::~JackAnalyzer() {
 }
 
 void JackAnalyzer::translate() {
+    if (paths_.empty()) throw file_exception("No .jack files");
     // 입력 데이터를 JackTokenizer 모듈로 토큰화한다.
     // 토큰들을 CompilationEngine 모듈에 전달해 컴파일한 후 출력 메시지를 전달한다.
 }
