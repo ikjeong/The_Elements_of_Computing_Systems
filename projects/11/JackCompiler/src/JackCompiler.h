e15308be850d106f5e4df027e6581dcd3d017600
jipark <jipark7937@gmail.com>
2022-04-30 13:42:13 +0000

    project11 - VMWriter로 대체되어야 하는 부분 주석 추가

--- projects/11/JackCompiler/src/JackCompiler.h
+++ projects/11/JackCompiler/src/JackCompiler.h
@@ -18,14 +18,17 @@ private:
     std::unique_ptr<JackTokenizer> jack_tokenizer_;
     std::unique_ptr<SymbolTable> symbol_table_;
     std::unique_ptr<CompilationEngine> compilation_engine_;
+    
     std::vector<std::string> paths_;
-    std::ofstream output_;
     std::string root_path_;
     
+    /* It need to be replaced. */
+    std::ofstream output_;
+    void setOutputFile(std::string path);
+
     void loadFilePaths(const std::string& path);
     bool isJackFile(const std::string& path) const;
     void compileFile(const std::string& path);
-    void setOutputFile(std::string path);
 
 public:
     JackCompiler(const std::string& path);
