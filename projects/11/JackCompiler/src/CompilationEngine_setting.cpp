ed92ade42fca196ce3a68b27b17a31a72299e7c5
jipark <jipark7937@gmail.com>
2022-05-06 05:36:27 +0000

    project11 - 변수 push & pop을 위한 함수 구현

--- projects/11/JackCompiler/src/CompilationEngine_setting.cpp
+++ projects/11/JackCompiler/src/CompilationEngine_setting.cpp
@@ -13,11 +13,23 @@
  * @param symbolTable Module that manages symbol tables.
  * @param output Specifies the file to be compiled output. It must be in the .xml file format.
  */
-void CompilationEngine::initialize(JackTokenizer* jackTokenizer, SymbolTable* symbolTable, std::ofstream* output) {
+void CompilationEngine::initialize(JackTokenizer* jackTokenizer, SymbolTable* symbolTable, VMWriter* vmWriter, const std::string& path) {
     jack_tokenizer_ = jackTokenizer;
     symbol_table_ = symbolTable;
-    output_ = output;
+    vm_writer_ = vmWriter;
+
+    std::string fileName = path.substr(0, path.find(".jack"));
+    for (int i = fileName.size()-1; i >= 0; --i) {
+        if (fileName[i] == '/') {
+            fileName.erase(0, i+1);
+            break;
+        }
+    }
+    file_name_ = fileName;
+
     indent_depth_ = 0;
+    while_label_index_ = 0;
+    if_label_index_ = 0;
 }
 
 /**
@@ -47,6 +59,56 @@ void CompilationEngine::checkAndDefineIdentifier(const std::string& type, const 
     else if (varKindInSymTable == VarKind::STATIC && varKind == VarKind::FIELD) throw compile_exception("This variable is already defined(static).", jack_tokenizer_->getCurrentTokenLineNumber());
 
     symbol_table_->define(jack_tokenizer_->getToken(), type, varKind);
+}
+
+/**
+ * If it is object(include array), push addres.
+ * else push value.
+ */
+void CompilationEngine::pushVariable(const VarKind varKind, const int index) {
+    switch (varKind) {
+    case VarKind::STATIC:
+        vm_writer_->writePush(Segment::STATIC, index);
+        break;
+    case VarKind::VAR:
+        vm_writer_->writePush(Segment::LOCAL, index);
+        break;
+    case VarKind::ARGUMENT:
+        vm_writer_->writePush(Segment::ARG, index);
+        break;
+    case VarKind::FIELD:
+        /* Current object's pointer push and pop */
+        vm_writer_->writePush(Segment::POINTER, 0);
+        vm_writer_->writePop(Segment::POINTER, 1);
+        
+        vm_writer_->writePush(Segment::THAT, index);
+        break;
+    default:
+        throw compile_exception("pushVariable(): Incorrect varKind.");
+    }
+}
+
+void CompilationEngine::popToVariable(const VarKind varKind, const int index) {
+    switch (varKind) {
+    case VarKind::STATIC:
+        vm_writer_->writePop(Segment::STATIC, index);
+        break;
+    case VarKind::VAR:
+        vm_writer_->writePop(Segment::LOCAL, index);
+        break;
+    case VarKind::ARGUMENT:
+        vm_writer_->writePop(Segment::ARG, index);
+        break;
+    case VarKind::FIELD:
+        /* Current object's pointer push and pop */
+        vm_writer_->writePush(Segment::POINTER, 0);
+        vm_writer_->writePop(Segment::POINTER, 1);
+        
+        vm_writer_->writePop(Segment::THAT, index);
+        break;
+    default:
+        throw compile_exception("popToVariable(): Incorrect varKind.");
+    }
 }
 
 /* =========== PUBLIC ============= */
